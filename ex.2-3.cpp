c1a85069418ee5ad3d6035a1b85acdd06a964012
annie0000000 <105290319+annie0000000@users.noreply.github.com>
2022-05-10 22:09:58 +0800

    Create ex.2-3.cpp

---
+++ ex.2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+       int     i;
+    double  f1;
+    char    s{30};
+    char    s1='c';
+   
+    printf("please input a integer \n");
+    scanf("%d", &i);
+}
