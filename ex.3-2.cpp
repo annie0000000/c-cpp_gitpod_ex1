df75016ce1c8f25cd4b70e34727225da117590b6
annie0000000 <105290319+annie0000000@users.noreply.github.com>
2022-05-10 22:20:25 +0800

    Create ex.3-2.cpp

---
+++ ex.3-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("please input x :\n");
+    scanf("%d",&x);
+    
+    printf("please input y :\n");
+    scanf("%d",&y);
+    printf("%d+%d=%d\n",x,y,x+y);
+    printf("%d-%d=%d\n",x,y,x-y);
+    printf("%d*%d=%d\n",x,y,x*y);
+    printf("%d/%d=%d\n",x,y,x/y);
+}
